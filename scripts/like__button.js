2d6206f5f7eb8cdd27d25715cc339bbdfe35380f
Maxim Kovalev <wolverin12@ya.ru>
2023-03-31 13:21:51 +0500

    Изменил структуру кода

--- scripts/like__button.js
+++ scripts/like__button.js
@@ -1,6 +1,6 @@
 let likes = document.querySelectorAll('.element__vector');
-likes.forEach(like => {
-like.addEventListener('click', function(e) {
-e.target.classList.toggle('element__vector_active');
-})
+likes.forEach(likes => {
+    likes.addEventListener('click', function(e) {
+        e.target.classList.toggle('element__vector_active');
+    }) 
 });
