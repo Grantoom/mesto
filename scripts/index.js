cbf67175168d9e8b3d146f8f8452cc67fa2c7237
Maxim Kovalev <wolverin12@ya.ru>
2023-06-27 17:52:00 +0500

    Добавил popup с возможностью добавлять карточки

--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,57 @@
-let editProfileBtn = document.querySelector('.profile__edit-button');
-let popup = document.querySelector('.popup');
-let editor = document.editor;
-let myname = editor.name;
-let about = editor.about;
-let description = document.querySelector('.profile__section-subtitle');
-let nickName = document.querySelector('.profile__section-title');
-let closeBtn = document.querySelector('.popup__exit');
+const editProfileBtn = document.querySelector('.profile__edit-button');
+const popup_editProfile = document.querySelector('.popup_edit-profile');
+const popup_addPhoto = document.querySelector('.popup_add-photo');
+const profileEditor = document.profileEditor;
+const cardsEditor = document.cardsEditor;
+
+const myname = profileEditor.nameEditProfile;
+const about = profileEditor.aboutEditProfile;
+const nickName = document.querySelector('.profile__section-title');
+const description = document.querySelector('.profile__section-subtitle');
+const closeBtnProfile = document.querySelector('.popup__exit_editProfile');
+const closeBtnPhoto = document.querySelector('.popup__exit_add-photo');
+const popup = document.querySelector('.popup');
+
+
+
+const addCardsBtn = document.querySelector('.profile__add-button');
+
+const cardsName = document.querySelector('.popup__input_NameCard');
+const cardsDescription = document.querySelector('.popup__input_UrlCard');
+
+addCardsBtn.addEventListener('click', function () {
+  popup_addPhoto.classList.add('popup_opened');
+});
 
 editProfileBtn.addEventListener('click', function () {
-    popup.classList.add('popup_opened');
+  popup_editProfile.classList.add('popup_opened');
     myname.value = nickName.textContent;
     about.value = description.textContent;
-})
+});
 
 function handleFormSubmit(evt) {
     evt.preventDefault();
     if (myname.value.length > 0 && about.value.length > 0) {
         nickName.textContent = myname.value;
         description.textContent = about.value;
-        popup.classList.remove('popup_opened');
+        popup_editProfile.classList.remove('popup_opened');
     } else {
         myname.setAttribute("placeholder", 'Введите имя');
         about.setAttribute("placeholder", 'Введите информацию о себе');
     }
-}
+};
 
-editor.addEventListener('submit', handleFormSubmit);
+profileEditor.addEventListener('submit', handleFormSubmit);
+cardsEditor.addEventListener('submit', handleCardSubmut);
 
-closeBtn.addEventListener('click', function () {
-    popup.classList.remove('popup_opened');
-})
+closeBtnProfile.addEventListener('click', () => {
+  popup_editProfile.classList.remove('popup_opened');
+});
+
+closeBtnPhoto.addEventListener('click', () => {
+  popup_addPhoto.classList.remove('popup_opened');
+});
+
 
 const initialCards = [
     {
@@ -60,18 +82,49 @@ const initialCards = [
 
 //контейнер
   const cardContainer = document.querySelector('.cardsPlace');
-//
   const cardTemplate = document.querySelector('#card-template').content;
 
+  showCards ();
 
-    initialCards.forEach ((item) => {
-    // клонируем содержимое тега template
-    const cardsElement = cardTemplate.querySelector('.element').cloneNode(true);
+    function handleCardSubmut (evt) {
+      
+      const nameInput = document.querySelector('#name');
+      const linkInput = document.querySelector('#link');
 
-    // наполняем содержимым
-    cardsElement.querySelector('.element__img').src = item.link;
-    cardsElement.querySelector('.element__text').textContent = item.name;
+      evt.preventDefault();
 
-    // отображаем на странице
-    cardContainer.append(cardsElement);
-    })
+      const item = {
+        name: nameInput.value,
+        link: linkInput.value
+      }
+      initialCards.unshift(item);
+      showCards();
+
+    };
+
+  function showCards () {
+
+    
+    let cards = initialCards.map ((item) => {
+      // клонируем содержимое тега template
+      const cardsElement = cardTemplate.querySelector('.element').cloneNode(true);
+      
+  
+      // наполняем содержимым
+      cardsElement.querySelector('.element__img').src = item.link;
+      cardsElement.querySelector('.element__text').textContent = item.name;
+  
+      //Ставить лайки
+      cardsElement.querySelector('.element__vector').addEventListener('click', (e) => {
+        e.target.classList.toggle('element__vector_active')
+    });
+  
+      // отображаем на странице
+      return cardsElement;
+
+      });
+
+      cardContainer.replaceChildren(...cards);
+
+  }
+  
