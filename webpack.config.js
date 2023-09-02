1f46c5e0e322814ff26ed6aabe8d573be8cce798
Maxim Kovalev <wolverin12@ya.ru>
2023-09-02 21:01:44 +0500

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,12 @@ module.exports = {
         test: /\.js$/,
         use: 'babel-loader',
         exclude: /node_modules/
-      }
+      },
+      {
+        // регулярное выражение, которое ищет все файлы с такими расширениями
+        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        type: 'asset/resource'
+      },
     ]
   },
   plugins: [
