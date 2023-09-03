b3916c6507a6640c28f7e9398f843561e1c8063c
Maxim Kovalev <wolverin12@ya.ru>
2023-09-03 13:51:50 +0500

    fix

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -3,8 +3,9 @@ import Popup from "./Popup.js";
 export default class PopupWithImage extends Popup {
   constructor(popupSelector) {
     super(popupSelector);
-    this._image = this._popup.querySelector(".popup__image");
-    this._caption = this._popup.querySelector(".popup__caption");
+    // Обновленные селекторы
+    this._image = this._popup.querySelector(".popup-image__pic");
+    this._caption = this._popup.querySelector(".popup-image__title");
   }
 
   open(link, name) {
