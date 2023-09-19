d18ab5667ad31eb2e56cc9d1f9f380e29286cb6b
Maxim Kovalev <wolverin12@ya.ru>
2023-09-19 22:18:44 +0500

    fix

--- src/components/PopupWithConfirmation.js
+++ src/components/PopupWithConfirmation.js
@@ -3,7 +3,7 @@ import Popup from "./Popup.js";
 export default class PopupWithConfirmation extends Popup {
   constructor(popupSelector, confirmCallback) {
     super(popupSelector);
-    this._confirmButton = this._popup.querySelector(".popup__save_type_delete");
+    this._confirmButton = this._popup.querySelector(".popup__type_delete");
     this._confirmCallback = confirmCallback;
   }
 
